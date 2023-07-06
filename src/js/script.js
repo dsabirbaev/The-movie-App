c5096eed8a72d147601ebfb1368fee941252a30c
Davranbek <davranbek_1995@mail.ru>
2023-07-07 02:25:00 +0500

    added modal window and local storage

--- src/js/script.js
+++ src/js/script.js
@@ -33,7 +33,7 @@ function renderAllMovies(data){
             const div = createElement('div', 'card bg-white shadow w-[290px] rounded-lg overflow-hidden', `
                 <img class="h-[250px] w-full object-cover object-center" src="${e.minImg}" alt="image">
                 <div class="card-body p-4">
-                    <h2 class="text-lg font-bold">${e.title}</h2>
+                    <h1 class="text-lg font-bold">${e.title}</h1>
                     <ul>
                         <li class="text-yellow-400"><strong class="text-black">Rating:</strong>
                             ${"<i class='bx bxs-star'></i>".repeat(Math.ceil(e.rating/2))};
@@ -44,11 +44,12 @@ function renderAllMovies(data){
                         <li><strong>Runtime:</strong> ${e.time}</li>
                         <li><strong>Category:</strong> ${e.categories}</li>
                     </ul>
-                    <button class="bg-cyan-700 text-white rounded-xl mt-4 me-3 focus:ring-2 ring-cyan-500 px-3 py-1">Details</button>
+                    <button class="btn_modal bg-cyan-700 text-white rounded-xl mt-4 me-3 focus:ring-2 ring-cyan-500 px-3 py-1">Details</button>
                     <a href="${e.link}" target="_blank" class="bg-red-700 text-white rounded-xl mt-4 me-3 focus:ring-2 ring-red-500 px-3 py-1">Details</a>
                 </div>
             `)
-
+            //// ID 
+            div.dataset.movie = e.id;
             $(".film-wrapper").append(div);
         })
     }else {
@@ -121,4 +122,60 @@ $('#search_btn').addEventListener('click', () => {
     const category = $('#category').value;
 
     findMovies(searchStr, rating, category);
+})
+
+
+/////////////// Events ////////////////////////
+
+function findElement(data, id){
+    return data.filter(item => item.id === id);
+}
+
+$(".film-wrapper").addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn_modal')){
+        const movieID = e.target.parentNode.parentNode.getAttribute('data-movie');
+        const result = findElement(allMovies, movieID)[0];
+        localStorage.setItem('movie', JSON.stringify(result));
+        $(".wrapper-modal").classList.toggle('hidden');
+        renderModal();
+    }
+})
+
+
+function renderModal(){
+    let {title, year, categories, lang, time, link, summary, rating, id, minImg, maxImg } = JSON.parse(localStorage.getItem('movie'))
+    let modalResult = "";
+    modalResult += `
+        <div class="grid-cols-2 grid p-4">
+            <img src="${minImg}" alt="pic" class="w-[400px] h-[250px] rounded-lg">
+
+        <ul class="text-2xl">
+            <li>
+                <h2><strong>Title: </strong>${title}</h2>
+            </li>
+            <li class="text-yellow-400"><strong class="text-black">Rating:</strong>
+                ${"<i class='bx bxs-star'></i>".repeat(Math.ceil(rating/2))};
+            </li>
+            <li><strong>Year:</strong>${year}</li>
+            <li><strong>Language:</strong>${lang}</li>
+            <li><strong>Runtime:</strong>${time}</li>
+            <li><strong>Category:</strong>${categories}</li>
+        </ul>
+        </div>
+        <div class="p-4">
+            <div class="bg-slate-300">
+                <p>${summary}</p>
+            </div>
+        </div>
+    `
+
+    $(".modal-center").innerHTML = modalResult;
+    
+}
+
+$("#close").addEventListener('click', () => {
+    $(".wrapper-modal").classList.add('hidden');
+})
+$("#cancel").addEventListener('click', () => {
+    $(".wrapper-modal").classList.add('hidden');
 })
